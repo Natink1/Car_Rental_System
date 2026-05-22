0ff9f8ca9635616ecd8e06d0ab751efddde67f6d
Natnael Deribe <88533508+Natink1@users.noreply.github.com>
2026-05-22 12:45:15 +0300

    User

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -10,7 +10,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            CarSeeder::class,
+            // CarSeeder::class,
         ]);
     }
 }
